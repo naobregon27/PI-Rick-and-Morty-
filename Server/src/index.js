16e4b0cc7aa98b30d7a5f9e0eedc1ccc35797184
naobregon27 <na.obregon27@hotmail.com>
2023-06-02 16:40:43 -0300

    PI modificado

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,38 +1,12 @@
-const express = require('express');
-const server = express();
+const server = require('./app')
 const PORT = 3001
-const morgan = require('morgan');
-const router = require('./Routes/index')
-
-
-server.use(express.json());
-server.use(morgan('dev'));
+const { conn } = require('./DB_connection');
 
 
 
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-     'Access-Control-Allow-Headers',
-     'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header(
-     'Access-Control-Allow-Methods',
-     'GET, POST, OPTIONS, PUT, DELETE'
-  );
-  next();
-});
-
-server.use('/rickandmorty', router );
-
-
-
-
-
+conn.sync({force: true})
 
 server.listen(PORT, ()=> console.log(`Listening on port: ${PORT}`))
-
 
 
 
